c424a581e8c89a0ff2c9c7949fc382a58755b32d
Mateus Cruz <mateuscolvr@gmail.com>
2023-09-23 10:49:25 -0300

    sum

--- main.ml
+++ main.ml
@@ -1,5 +1,7 @@
 open Ast
 
+let todo () = failwith "TODO"
+
 (** [parse s] parses [s] into an AST. *)
 let parse (s : string) : expr =
   let lexbuf = Lexing.from_string s in
@@ -11,16 +13,27 @@ let parse (s : string) : expr =
 let string_of_val (e : expr) : string =
   match e with
   | Int i -> string_of_int i
+  | Binop _ -> failwith "precondition violated"
 ;;
 
 (** [is_value e] is whether [e] is a value. *)
 let is_value : expr -> bool = function
   | Int _ -> true
+  | Binop _ -> false
 ;;
 
 (** [step e] takes a single step of evaluation of [e]. *)
 let rec step : expr -> expr = function
   | Int i -> failwith "Does not step"
+  | Binop (bop, e1, e2) when is_value e1 && is_value e2 -> step_bop bop e1 e2
+  | Binop (bop, e1, e2) when is_value e1 -> Binop (bop, e1, step e2)
+  | Binop (bop, e1, e2) -> Binop (bop, step e1, e2)
+
+(** [step_bop bop v1 v2] implements the primitive operation [v1 bop v2]. Requires [v1] and [v2] are both values. *)
+and step_bop bop v1 v2 =
+  match bop, v1, v2 with
+  | Add, Int a, Int b -> Int (a + b)
+  | _ -> failwith "precondition violated"
 ;;
 
 (** [eval e] fully evaluates [e] to a value [v]. *)
--- test.ml
+++ test.ml
@@ -5,5 +5,5 @@ open Main
 (** [make_i n i s] makes an OUnit test named [n] that expects [s] to evaluate to [i]. *)
 let make_i n i s = n >:: fun _ -> assert_equal (string_of_int i) (interp s)
 
-let tests = [ make_i "int" 22 "22" ]
+let tests = [ make_i "int" 22 "22"; make_i "add" 22 "11+11" ]
 let _ = run_test_tt_main ("suite" >::: tests)
