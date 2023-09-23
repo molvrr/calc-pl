0b3f84ef72340d8c3db40ae5b4e300b3d2e194cb
Mateus Cruz <mateuscolvr@gmail.com>
2023-09-23 10:22:15 -0300

    integer parsing

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs.nixpkgs.url = "github:nix-ocaml/nix-overlays";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs;
+            with ocamlPackages; [
+              dune_3
+              findlib
+              ocaml
+              ocaml-lsp
+              ocamlformat
+              ocamlformat-rpc-lib
+              utop
+              menhir
+            ];
+        };
+      });
+}
---
+++ parser.mly
@@ -0,0 +1,14 @@
+%token <int> INT
+%token EOF
+
+%start <Ast.expr> prog
+
+%%
+
+prog:
+  | e = expr; EOF { e }
+  ;
+
+expr:
+  | i = INT { Int i }
+  ;
